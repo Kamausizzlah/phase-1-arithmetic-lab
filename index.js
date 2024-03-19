e6b07a8a8fbf9fb15f9d2dc01c815088161dbcb1
ian kamau <iankamau112@gmail.com>
2024-03-19 13:00:03 +0300

    arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let num3 = 18;
+let num4 = 7;
+let mod;
+mod = num3 % num4;
+console.log(mod);
+
+let max;
+max = Math.max(15, 16, 17, 18, 19, 20);
+console.log(max);
